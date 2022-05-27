ebd214bde9b46bbe11640f77b2b2697c081c194a
Fred Higgins <higgins.fred.s@gmail.com>
2022-05-26 18:09:53 -0700

    completed

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,15 @@
 class StudentsController < ApplicationController
 
   def index
+    byebug
     students = Student.all
+    # student = Student.find_by(last_name: params[:name].capitalize)
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
